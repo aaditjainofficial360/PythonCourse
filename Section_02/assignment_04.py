92b16dd8648351a18361e48737630ff5592d28d3
aaditjainofficial360 <105768739+aaditjainofficial360@users.noreply.github.com>
2023-07-09 23:14:15 +0530

    Submitted

--- Section_02/assignment_04.py
+++ Section_02/assignment_04.py
@@ -9,8 +9,12 @@ my_list = [(1, 2), (3, 4), (['c', 'd', 'a', 'm'], [3, 9, 4, 12], 4), 'TV', 42]
 
 # Your Code Below:
 
-
-
+third_element=list(my_list[2])
+manipulation_1=third_element[0][3]='x'
+third_element=tuple(third_element)
+fourth_element=my_list[3]='television'
+my_list.pop(2)
+my_list.insert(2,third_element)
 
 
 
